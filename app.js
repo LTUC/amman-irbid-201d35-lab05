1825d45296318bf91af49e5f9d195b3e2c1366ab
tariqkjm7 <d.alatoum@gmail.com>
2021-08-05 13:29:18 +0300

    problrm4 solved

--- app.js
+++ app.js
@@ -48,10 +48,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
+    let sum1 = a+b+c;
+    let multi1 = a*b*c;
+    let msg1 = `${a} and ${b} and ${c} sum to ${sum1}.`
+    let msg2 = `The product of ${a} and ${b} and ${c} is ${multi1}.`
+return [sum1,multi1,msg1,msg2]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -66,14 +71,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
+
 let testArray = [2, 3, 4];
 
-function sumArray(sumArr) {
+function sumArray(testArray) {
+
+let sum = 0;
+for(let i =0; i<testArray.length; i++){
+    sum=sum+testArray[i];
+}
+let msg = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${sum} is their sum.`
+return[sum, msg]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
