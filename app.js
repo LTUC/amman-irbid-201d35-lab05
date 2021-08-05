8314b57b2a832ff93d7d7191d723ad9e48a691f1
Qamar Alkhatib <qalkhatib0@gmail.com>
2021-08-05 16:16:13 +0300

    problem4 Solved

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ function sumAndMultiply(a, b, c) {
    let smessage = `The product of ${a} and ${b} and ${c} is ${mult}.`
 
    return [suum, mult, fmessage,smessage];
-
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
@@ -78,11 +77,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4];
 
 function sumArray(sumArr) {
+    let sum = 0;
+    for (let i = 0; i < sumArr.length; i++){
+        sum = sum + sumArr[i];
+    }
+
+    let message = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum} is their sum.`
+    return [sum,message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
