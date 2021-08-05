f8dd39c0e8bd2744d28a9471bedb112d211aae07
Lawrence AbuDubai <EstarRave203@gmail.com>
2021-08-05 12:42:26 +0300

    problem3 solved

--- app.js
+++ app.js
@@ -48,10 +48,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
+    
+    let sum3 = sum(a, b)[0];
+    sum3 = sum(sum3, c)[0];
+    let multiply3 = multiply(a, b)[0];
+    multiply3 = multiply(multiply3, c)[0];
+    let message3rd = `${a} and ${b} and ${c} sum to ${sum3}.`;
+    let message4th = `The product of ${a} and ${b} and ${c} is ${multiply3}.`;
+    return [sum3, multiply3, message3rd, message4th];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
