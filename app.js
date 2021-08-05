9433b832ebb03cc11823762396f98714cf6196d1
YasmeenKelani <yasminekailani98@gmail.com>
2021-08-05 14:33:09 +0300

    problem2 solved

--- app.js
+++ app.js
@@ -27,10 +27,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
+    let mu = a*b;
+    let msg =`The product of ${a} and ${b} is ${mu}.`;
+    return [mu,msg];
+
 }
 
+
+
+
+
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -46,6 +54,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
+     
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
