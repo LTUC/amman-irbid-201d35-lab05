b08569571e552f7c91436d9906cb0cf503e7a756
Abdallah-Abu-Hussein <abdallh1133224466@gmail.com>
2021-08-05 14:11:11 +0300

    problem1 solved

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
     let sum = a + b;
-    let massage = 'The sum of '+a+' and ' +b+' is '+sum+'.'
-    ;
-    return [sum,massage]
+    let massage = 'The sum of '+a+' and ' +b+' is '+sum+'.' ;
+    return [sum,massage];
 }
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
