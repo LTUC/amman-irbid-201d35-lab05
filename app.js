a74d382f34da875764efa9d3608efcf910a76f8a
EmadIdris <emadidris@outlook.com>
2021-08-05 12:27:11 +0300

    Solve Problem3

--- app.js
+++ app.js
@@ -48,10 +48,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
+    let sum =a+b+c;
+    let mull = a*b*c;
+    let mess1=`${a} and ${b} and ${c} sum to ${sum}.`
+    let mess2=`The product of ${a} and ${b} and ${c} is ${mull}.`
+    return [sum,mull,mess1,mess2]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
