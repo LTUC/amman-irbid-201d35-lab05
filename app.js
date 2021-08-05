93b612acee2d88ab803f8675051cd93e09bd9b50
haneen <haneenjaradeh1@gmail.com>
2021-08-05 14:20:26 +0300

    changes in problem2

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ let message = `The product of ${a} and ${b} is ${multi}.`
 return [multi , message];
 }
 
+
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
