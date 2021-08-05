8e74c490f92ddb80afd802904a1758cf2d4ab34f
Ziyadhs <ziyadhs2001@gmail.com>
2021-08-05 16:13:39 +0300

    solve problem 3

--- app.js
+++ app.js
@@ -47,12 +47,25 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// Write your code here
+// Write your code here;
+function sumFunP3(as, bs){
+    let sum = as + bs;
+    return sum;
+}
+function multiFunP3(am, bm) {
+    let multi = am * bm;
+    return multi;
+}
 function sumAndMultiply(a, b, c) {
+    let sum = sumFunP3(sumFunP3(a, b) ,c);
+    let multi = multiFunP3(multiFunP3(a, b), c);
+    let messageSum = `${a} and ${b} and ${c} sum to ${sum}.`;
+    let messageMulti = `The product of ${a} and ${b} and ${c} is ${multi}.`;
+    return [sum, multi, messageSum, messageMulti]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
