2b9e9a432f44cd5689d034dd2d6401ffb83099b7
YasmeenKelani <yasminekailani98@gmail.com>
2021-08-05 15:09:26 +0300

    problem3

--- app.js
+++ app.js
@@ -54,12 +54,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-     
+
+    var firstSum = sum(a, b)[0];
+    var totalSum = sum(firstSum, c)[0];
+    var firstmul = multiply(a, b)[0];
+    var totalmul = multiply(firstmul, c)[0];
+    var message1 = `${a} and ${b} and ${c} sum to ${totalSum}.`;
+    var msg2 = `The product of ${a} and ${b} and ${c} is ${totalmul}.`;
+    var newArray = [totalSum, totalmul, message1, msg2];
+    return newArray;
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
