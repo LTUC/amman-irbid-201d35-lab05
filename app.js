705ee39c0b2654af00aceadb12bbfa308f4f11d9
Abdallah-Abu-Hussein <abdallh1133224466@gmail.com>
2021-08-05 17:01:56 +0300

    fixnig problem4

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) {
     let sum = a + b;
-    return sum;
+    let massage = 'The sum of '+a+' and ' +b+' is '+sum+'.' ;
+    console.log(sum);
+    return [sum,massage];
 }
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -66,10 +68,10 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4];
 
 function sumArray(sumArr) {
-let sum1 = sum(sumArr[0],sumArr[1]);
-let sum2 = sum(sum1,sumArr[2]);
+let sum1 = sum(sumArr[0],sumArr[1])[0];
+let sum2 = sum(sum1,sumArr[2])[0];
 let message = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum2} is their sum.`
-
+//console.log(sum2);
 return [sum2,message];
 
 }
