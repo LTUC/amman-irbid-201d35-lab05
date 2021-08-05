4a880667c0e389da9261269867241c4ba618189f
leenahmad <leenkaraja24@gmail.com>
2021-08-05 17:55:35 +0300

    problem5 solved

--- app.js
+++ app.js
@@ -108,11 +108,25 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) {
+let Array1 = [2, 3, 4]; 
+
+function multiplyArray(multArr){
+    let multiply = 1;
+
+    for(let i = 0; i < Array1.length; i++){
+ 
+        multiply *= Array1[i];
+    }
+
+    let message = `The numbers ${Array1[0]},${Array1[1]},${Array1[2]} have a product of ${multiply}.`
+
+    return[multiply , message];
+    
+ 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(Array1);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
