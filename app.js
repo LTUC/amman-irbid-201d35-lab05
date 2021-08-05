e3567ea6e73159e719e172eeaa18ac5fc5b57a95
Saif-K-Saeed <saif.k.saeed@gmail.com>
2021-08-05 14:36:51 +0300

    problem5 solved

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function sumArray(sumArr) {
 
     let arrayMessage = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${sumOfArray} is their sum.`
     return [sumOfArray, arrayMessage];
-    
+
 }
 
 
@@ -106,20 +106,33 @@ testSumArray(testArray);
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiplyArray() that takes an array of numbers as its argument and returns 
+an array whose first element is the product of those numbers, and the second element is a string that 
+EXACTLY follows this example and uses the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication,
+ use your multiply() function that you've already created. You're going to have to be resourceful to figure 
+ out how to do this. This function should handle an array containing three elements. However, you may continue 
+ to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the 
+call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
 function multiplyArray(multArr) {
+    let multArr1 = 1;
+    for (let i = 0; i < testArray.length; i++) {
+        multArr1 *= testArray[i];
+
+    }
+    let multArrayMessage = `The numbers ${testArray[0]},${testArray[1]},${testArray[2]} have a product of ${multArr1}.`
+    return [multArr1,multArrayMessage];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
