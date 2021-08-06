87567ec9700c71c31ae8be53d4748b3748674248
ammarzeyad <ammarramadan16@yahoo.com>
2021-08-07 01:22:37 +0300

    problem6 solved

--- app.js
+++ app.js
@@ -130,9 +130,19 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1, 2, 3, 4, 5];
 
 function multiplyAnyArray(dynamicArray) {
+    let a = 1;
+    let message = "The numbers ";
+    for(let i=0; i<dynamicArray.length;i++){
+        a = multiply(a,dynamicArray[i])[0];
+        message += dynamicArray[i];
+        if(i < dynamicArray.length-1){
+            message += ",";
+        }
+ }
+ return[a,message + ` have a product of ${a}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
