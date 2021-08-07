186bc48252e6163ca07a62241c2c81cd0eae288d
AliMwas <alimwas61@gmail.com>
2021-08-07 12:56:58 +0300

    problem2 solved

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function sum(a, b) {
 
     let sum = a + b;
     let message = `The sum of ${a} and ${b} is ${sum}.`;
-    return [sum, Message];
+    return [sum, message];
 }
 // Here is the test for sum(); uncomment it to run it
  testSum(4, 7);
