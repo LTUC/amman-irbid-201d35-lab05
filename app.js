681e626826cc1186b9f543d4087a06b21c5c15c2
Qamar <qalkhatib0@gmail.com>
2021-08-07 15:12:51 +0300

    problem5 solved

--- app.js
+++ app.js
@@ -104,10 +104,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
+    let mult = 1;
+    for (let i = 0; i < multArr.length; i++){
+        mult = mult * multArr[i];
+    }
+    let message = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${mult}.`
+    return [mult, message];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
