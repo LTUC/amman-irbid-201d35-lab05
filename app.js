210aac5253db67c0b3dde766242dae49c0cdddb2
Bashar-Owainat <owainatbashar13@gmail.com>
2021-08-09 07:24:27 +0300

    edited problem 3,4 and 5

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ function multiply(a, b) {
     return [product, message];
 }
 
+
+
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -48,17 +50,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// Write your code here
+//Write your code here
 function sumAndMultiply(a, b, c) {
-    let sum = a + b + c;
-    let product = a * b * c;
-    let element3 =  `${a} and ${b} and ${c} sum to ${sum}.`;
-    let element4 = `The product of ${a} and ${b} and ${c} is ${product}.`;
-    return [sum, product, element3, element4];
+    let sumAB  = sum(a,b)[0];
+    let sumABC = sum(sumAB,c)[0];
+    let productAB = multiply(a,b)[0];
+    let productABC = multiply(productAB,c)[0];
+    let element3 =  `${a} and ${b} and ${c} sum to ${sumABC}.`;
+    let element4 = `The product of ${a} and ${b} and ${c} is ${productABC}.`;
+    return [sumABC, productABC, element3, element4];
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
+//Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -76,9 +81,10 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4];
 
 function sumArray(sumArr) {
-    let sum = sumArr[0] + sumArr[1] + sumArr[2];
-    let message = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum} is their sum.`;
-    return [sum, message];
+    let sum01 = sum(sumArr[0], sumArr[1] )[0];
+    let sum012 = sum(sum01, sumArr[2])[0];
+    let message = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sum012} is their sum.`;
+    return [sum012, message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -99,9 +105,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
-    let product = multArr[0] * multArr[1] *  multArr[2];
-    let message = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${product}.`
-    return [product, message];
+    let product01 = multiply(multArr[0], multArr[1])[0];
+    let product012 = multiply(product01, multArr[2])[0];
+    let message = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${product012}.`
+    return [product012, message];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
