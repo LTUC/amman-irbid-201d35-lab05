eead05693a3eb55b34cbcc375f44c7b6d9c190f1
Mohammad-01 <moalhasan432@gmail.com>
2021-08-10 20:16:36 +0300

    edit

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) {
+function sum(a, b) { 
+    let sum = a + b;
+    let massage = `The sum of ${a} and ${b} is ${sum}.`; 
+    return [sum, massage];
 }
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -23,10 +27,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
+    let multi = a * b;
+    let message = `The product of ${a} and ${b} is ${multi}.`;
+    return [multi, message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -42,9 +49,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-    let sum = a + b + c;
-    let multiply = a * b * c;
-    return [sum , multiply ,`${a} and ${b} and ${c} sum to ${sum}.` , `The product of ${a} and ${b} and ${c} is ${multiply}.` ];
+
+    let Sum = sum (c , sum(a, b)[0])[0];
+    let multi = multiply( c , multiply(a, b)[0])[0];
+
+    return [Sum , multi ,`${a} and ${b} and ${c} sum to ${Sum}.` , `The product of ${a} and ${b} and ${c} is ${multi}.` ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
